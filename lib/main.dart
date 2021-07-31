ab6d7b580b919bbff08217df28d69bea496e3ed8
SilentCruzer <radithyakumar7@gmail.com>
2021-07-31 12:12:44 +0530

    Added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:metaville/screens/constants.dart';
-import 'package:metaville/screens/loginscreen.dart';
+import 'package:metaville2/screens/constants.dart';
+import 'package:metaville2/screens/loginscreen.dart';
 
 void main() {
   runApp(ScreenUtilInit(
